af4d787dfa9911e96e066e23c4bb5be0f8048010
PatriziaCrimi <crimi.patrizia@gmail.com>
2021-01-20 18:50:14 +0100

    HomeController created & Header-Navbar added

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-
-// Route::get('/product', function () {
-//     return view('product')->name('product');
-// });
+Route::get('/', 'HomeController@index')->name('homepage');
 
 Route::resource('/products', 'ProductController');
